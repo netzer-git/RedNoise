3597c5b5ddc52b05c307c2c6cbe789f21cb16603
Netzer <netzer.epstein@mail.huji.ac.il>
2022-10-03 14:15:58 +0100

    Task 5 complete

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -31,13 +31,20 @@ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 t
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
-	std::vector<float> result;
-	result = interpolateSingleFloats(254, 0, WIDTH);
+	std::vector<glm::vec3> result;
+	glm::vec3 topLeft(255, 0, 0);        // red 
+	glm::vec3 topRight(0, 0, 255);       // blue 
+	glm::vec3 bottomRight(0, 255, 0);    // green 
+	glm::vec3 bottomLeft(255, 255, 0);   // yellow
+
 	for (size_t y = 0; y < window.height; y++) {
+		glm::vec3 from(255, 0 + y, 0);
+		glm::vec3 to(0, 0 + y, 255 - y);
+		result = interpolateThreeElementValues(from, to, WIDTH);
 		for (size_t x = 0; x < window.width; x++) {
-			float red = result[x];
-			float green = result[x];
-			float blue = result[x];
+			float red = result[x].x;
+			float green = result[x].y;
+			float blue = result[x].z;
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
 			window.setPixelColour(x, y, colour);
 		}
@@ -59,14 +66,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
-
-	glm::vec3 from(1.0, 4.0, 9.2);
-	glm::vec3 to(4.0, 1.0, 9.8);
-	std::vector<glm::vec3> result;
-	result = interpolateThreeElementValues(from, to, 4);
-	for (size_t i = 0; i < result.size(); i++) std::cout << result[i].x << " " << result[i].y << " " << result[i].z << " ";
-	std::cout << std::endl;
-
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
